bfb2f52404ba22a489d53467d1c60ac3105f057b
Xinchen Liu <mike000708@me.com>
2022-09-07 20:45:49 -0400

    resolve user for twitter

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,8 +1,19 @@
-export const supportedNetworks = [
-  {
-    name: "twitter",
-    logo: "icon-park:twitter",
-    apiKey: import.meta.env.TWITTER_TOKEN,
-    apiUrl: "https://api.twitter.com/2",
+const twitter = {
+  name: "twitter",
+  logo: "icon-park:twitter",
+  apiKey: import.meta.env.TWITTER_TOKEN,
+  apiUrl: "https://api.twitter.com/2",
+  get resolveUser() {
+    return async (userName: string) => {
+      return fetch(
+        `${this.apiUrl}/users/by/username/${userName}?user.fields=description,profile_image_url`,
+        {
+          method: "GET",
+          headers: { Authorization: `Bearer ${this.apiKey}` },
+        }
+      ).then((response) => response.json());
+    };
   },
-];
+};
+
+export const supportedNetworks = [twitter];
