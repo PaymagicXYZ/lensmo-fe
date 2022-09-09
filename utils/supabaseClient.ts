530f6367814876c89a8115bc4299a70a60ab943c
Xinchen Liu <mike000708@me.com>
2022-09-09 12:06:16 -0400

    enable oauth signin with supabase

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -2,6 +2,14 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://rvhpnxjvpgatvgaubbyt.supabase.co";
 const supabaseKey = import.meta.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
+const options = {
+  schema: "public",
+  headers: { "x-my-custom-header": "lensmo" },
+  autoRefreshToken: true,
+  persistSession: true,
+  detectSessionInUrl: true,
+};
+
+const supabase = createClient(supabaseUrl, supabaseKey, options);
 
 export default supabase;
