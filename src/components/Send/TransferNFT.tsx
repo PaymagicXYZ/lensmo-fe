1689a740835c6b5a18fd015be4a13ef1bdb9b24b
Xinchen Liu <mike000708@me.com>
2022-09-12 00:07:34 -0400

    add tabs for NFT

---
+++ src/components/Send/TransferNFT.tsx
@@ -0,0 +1,28 @@
+import { Web3Wrapper } from "../Web3Wrapper";
+import { useAccount, useNetwork } from "wagmi";
+
+const Wallet = () => {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  const { chain } = useNetwork();
+  return (
+    <>
+      {isConnecting && <div>Connecting...</div>}
+      {isDisconnected && <div>Please connect your wallet to continue.</div>}
+      {address && (
+        <form className="form-control">
+          <label className="label">
+            <span className="label-text">NFT Collection</span>
+          </label>
+        </form>
+      )}
+    </>
+  );
+};
+
+export const TransferNFT = () => {
+  return (
+    <Web3Wrapper>
+      <Wallet />
+    </Web3Wrapper>
+  );
+};
