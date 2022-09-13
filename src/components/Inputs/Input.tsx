a51c56408387ecb31321dc572fe5ca9a79ea9859
Xinchen Liu <mike000708@me.com>
2022-09-13 19:46:40 -0400

    update input

--- src/components/Inputs/Input.tsx
+++ src/components/Inputs/Input.tsx
@@ -1,20 +1,22 @@
 export const Input = (props: {
-  label: string;
+  label?: string;
   placeholder: string;
   rightIcon?: JSX.Element | string;
 }) => {
   return (
     <>
-      <label className="label">
-        <span className="label-text">{props.label}</span>
-      </label>
+      {props.label && (
+        <label className="label">
+          <span className="label-text">{props.label}</span>
+        </label>
+      )}
       <label className="input-group">
         <input
           type="text"
           placeholder={props.placeholder}
           className="input input-bordered"
         />
-        <span>{props.rightIcon}</span>
+        {props.rightIcon && <span>{props.rightIcon}</span>}
       </label>
     </>
   );
