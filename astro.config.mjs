c1c787ef0ced98fed1fc776c87228223a35d064d
Xinchen Liu <mike000708@me.com>
2022-10-03 14:24:26 -0400

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), partytown()],
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
